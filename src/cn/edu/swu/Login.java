792a45bc2ef813d3f578f3faca7d034c4d64264d
MasterCorner <MasterCorner@users.noreply.github.com>
2019-03-17 18:55:54 +0800

    Changed a good looking template, but failed to build the database, so it ended temporarily

--- src/cn/edu/swu/Login.java
+++ src/cn/edu/swu/Login.java
@@ -1,14 +1,22 @@
 package cn.edu.swu;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class Login extends HttpServlet{
 
-	public void doPost(HttpServletRequest req,HttpServletResponse resp) {
+	public void doPost(HttpServletRequest req,HttpServletResponse resp) throws IOException {
 		System.out.println(req.getContextPath());
 
 		String user = req.getParameter("user");
 		String pass = req.getParameter("pass");
+		
 		try{
 			System.out.println(new String(user.getBytes("ISO-8859-1"), "UTF-8"));
 		} catch(Exception e) {
@@ -16,10 +24,16 @@ public class Login extends HttpServlet{
 		}
 		System.out.println(pass);
 		System.out.println("test");
+		File file =new File("user.txt");
+		Writer out =new FileWriter(file);
+		out.write(user);
+		out.write(pass);
+		out.close();
+
 	}
 
 
-	public void doGet(HttpServletRequest req, HttpServletResponse resp){
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		doPost(req, resp);
 	}
 }
