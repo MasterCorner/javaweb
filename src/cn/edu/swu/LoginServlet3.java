9d7c77fe7ecc0462dc0b5c4e1b5237c756471288
MasterCorner <MasterCorner@users.noreply.github.com>
2019-03-24 16:53:26 +0800

    访问远程数据库
    经过三天坚持不懈的百度和尝试，终于可以访问远程数据库而非本地数据库了，这是和上一个版本唯一的区别。解决方法请参考https://blog.csdn.net/b296405422/article/details/88778185

--- src/cn/edu/swu/LoginServlet3.java
+++ src/cn/edu/swu/LoginServlet3.java
@@ -1,11 +1,16 @@
 package cn.edu.swu;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,6 +23,7 @@ public class LoginServlet3 extends HttpServlet{
 	protected void doPost(HttpServletRequest req,HttpServletResponse resp)throws ServletException,IOException{
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
+		String userip = req.getRemoteAddr();
 		
 		Connection connection = null;
 		PreparedStatement statement = null;
@@ -25,10 +31,23 @@ public class LoginServlet3 extends HttpServlet{
 		
 		PrintWriter out = resp.getWriter();
 		
+		//写入文件模块
+		File file =new File("webuser.txt");
+		FileWriter fw=null;
+		fw=new FileWriter(file.getAbsoluteFile(),true);//true表示续写而不是覆盖
+		BufferedWriter bw=new BufferedWriter(fw);
+		bw =new BufferedWriter(fw);
+		SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		bw.write("systime:"+df.format(new Date())+"\r\n");
+		bw.write("username:"+username+"\r\n");//win用\r\n，linux用\n，Mac用\r
+		bw.write("password:"+password+"\r\n");
+		bw.write("user_ip:"+userip+"\r\n\r\n");
+		bw.close();
+		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");//12.13
-			//String url = "jdbc:mysql://47.102.203.124:3306/swu_db?characterEncoding=utf-8&serverTimezone=UTC";
-			String url = "jdbc:mysql://localhost:3306/swu_db?characterEncoding=utf-8&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
+			String url = "jdbc:mysql://47.102.203.124:3306/swu_db?characterEncoding=utf-8&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
+			//String url = "jdbc:mysql://localhost:3306/swu_db?characterEncoding=utf-8&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
 			String user = "root";
 			String pass = "network";
 			connection = DriverManager.getConnection(url,user,pass);
